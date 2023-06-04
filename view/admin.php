30d5733c01c5eb47e95cb4c76c17650cecb63c76
YasserAF2 <9087@cifpceuta.es>
2023-06-04 18:33:37 +0200

    arreglo diseño

--- view/admin.php
+++ view/admin.php
@@ -2,22 +2,35 @@
 echo $_SESSION['usuario'];
 ?>
 <main class="admin">
-    <div>
-        <div>
-            <h2>Lista de opciones:</h2>
-            <ul>
-                <li><a href="index.php?action=lista_usuarios" class="opciones-lista">Lista de Usuarios</a></li>
-                <li><a href="index.php?action=ver_peticiones" class="opciones-lista">Gestionar Peticiones</a></li>
-            </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Lista de opciones:</h2>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <a href="index.php?action=lista_usuarios" class="opciones-lista">Lista de
+                            Usuarios</a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="index.php?action=ver_peticiones" class="opciones-lista">Gestionar
+                            Peticiones</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row justify-content-between align-items-center w-50">
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <a href="index.php" class="btn btn-primary">Volver al perfil de usuario</a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <form class="cerrar" method="post" action="index.php?action=logout">
+                        <input type="submit" class="btn btn-danger" value="Cerrar sesión">
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-    <div>
-        <a href="index.php" class="btn btn-primary">Volver al perfil de usuario</a>
-    </div>
-    <div>
-        <form class="cerrar" method="post" action="index.php?action=logout">
-            <input type="submit" value="Cerrar sesión">
-        </form>
-    </div>
-
 </main>
--- view/lista_usuarios.php
+++ view/lista_usuarios.php
@@ -1,58 +1,59 @@
 <?php
 $empleados = $dataToView['empleados'];
 ?>
+<main class="emp">
+    <div class="empleados-lista">
+        <h2>Lista y administración de los empleados.</h2>
+        <button class="volver" onclick="window.location.href='index.php?action=admin'"><i class="fas fa-arrow-left"></i>
+            Volver a la
+            página principal</button>
 
-<div class="empleados-lista">
-    <h2>Lista y administración de los empleados.</h2>
-    <button class="volver" onclick="window.location.href='index.php?action=admin'"><i class="fas fa-arrow-left"></i>
-        Volver a la
-        página principal</button>
-
-    <table class="tabla-empleados">
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Primer Apellido</th>
-                <th>Segundo Apellido</th>
-                <th>DNI</th>
-                <th>Código ZKT</th>
-                <th>Dirección</th>
-                <th>Email</th>
-                <th>País</th>
-                <th>Ciudad</th>
-                <th>Provincia</th>
-                <th>Código Postal</th>
-                <th>Teléfono Fijo</th>
-                <th>Teléfono Móvil</th>
-
-                <th>Editar Usuario</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($empleados as $empleado) : ?>
+        <table class="tabla-empleados">
+            <thead>
                 <tr>
-                    <td><?php echo $empleado->getNOMBRE(); ?></td>
-                    <td><?php echo $empleado->getAPELLIDO_1(); ?></td>
-                    <td><?php echo $empleado->getAPELLIDO_2(); ?></td>
-                    <td><?php echo $empleado->getDNI(); ?></td>
-                    <td><?php echo $empleado->getCodZkt(); ?></td>
-                    <td><?php echo $empleado->getDireccion(); ?></td>
-                    <td><?php echo $empleado->getEMAIL(); ?></td>
-                    <td><?php echo $empleado->getPais(); ?></td>
-                    <td><?php echo $empleado->getCiudad(); ?></td>
-                    <td><?php echo $empleado->getProvincia(); ?></td>
-                    <td><?php echo $empleado->getCp(); ?></td>
-                    <td><?php echo $empleado->getTelfCasa(); ?></td>
-                    <td><?php echo $empleado->getTlfMovil(); ?></td>
-                    <td>
-                        <?php $dni = $empleado->getDNI(); ?>
-                        <form class="editar2" method="post" action="index.php?action=editar_empleado">
-                            <input type="hidden" name="dni" value="<?php echo $dni; ?>">
-                            <input type="submit" value="Editar">
-                        </form>
-                    </td>
+                    <th>Nombre</th>
+                    <th>Primer Apellido</th>
+                    <th>Segundo Apellido</th>
+                    <th>DNI</th>
+                    <th>Código ZKT</th>
+                    <th>Dirección</th>
+                    <th>Email</th>
+                    <th>País</th>
+                    <th>Ciudad</th>
+                    <th>Provincia</th>
+                    <th>Código Postal</th>
+                    <th>Teléfono Fijo</th>
+                    <th>Teléfono Móvil</th>
+
+                    <th>Editar Usuario</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                <?php foreach ($empleados as $empleado) : ?>
+                    <tr>
+                        <td><?php echo $empleado->getNOMBRE(); ?></td>
+                        <td><?php echo $empleado->getAPELLIDO_1(); ?></td>
+                        <td><?php echo $empleado->getAPELLIDO_2(); ?></td>
+                        <td><?php echo $empleado->getDNI(); ?></td>
+                        <td><?php echo $empleado->getCodZkt(); ?></td>
+                        <td><?php echo $empleado->getDireccion(); ?></td>
+                        <td><?php echo $empleado->getEMAIL(); ?></td>
+                        <td><?php echo $empleado->getPais(); ?></td>
+                        <td><?php echo $empleado->getCiudad(); ?></td>
+                        <td><?php echo $empleado->getProvincia(); ?></td>
+                        <td><?php echo $empleado->getCp(); ?></td>
+                        <td><?php echo $empleado->getTelfCasa(); ?></td>
+                        <td><?php echo $empleado->getTlfMovil(); ?></td>
+                        <td>
+                            <?php $dni = $empleado->getDNI(); ?>
+                            <form class="editar2" method="post" action="index.php?action=editar_empleado">
+                                <input type="hidden" name="dni" value="<?php echo $dni; ?>">
+                                <input type="submit" value="Editar">
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</main>
