2fcefadf8a24f41a22a2aea861eda08606cf500c
YasserAF2 <yas.123af@gmail.com>
2024-06-26 10:57:22 +0200

    arreglo

--- view/resultado_asuntos_propios.php
+++ view/resultado_asuntos_propios.php
@@ -13,20 +13,25 @@ if (!isset($_SESSION['correo'])) {
 ?>
 
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 offset-md-2 content">
-            <?php
-            // Verificar si la variable $resultado está definida en la sesión
-            if (isset($_SESSION['resultado_solicitud'])) {
-                // Mostrar el mensaje de resultado
-                echo "<h1 class='text-center'>{$_SESSION['resultado_solicitud']}</h1>";
-            } else {
-                // Si la variable $resultado no está definida, mostrar un mensaje de error
-                echo "<h1 class='text-center'>No se ha recibido ningún resultado.</h1>";
-            }
-            ?>
-            <div class="text-center mt-3">
-                <a href="index.php?action=solicitud_asuntos_propios" class="btn btn-primary">Volver a la página de solicitud de asuntos propios</a>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card mt-5">
+                <div class="card-body text-center">
+                    <?php
+                    // Verificar si la variable $resultado está definida en la sesión
+                    if (isset($_SESSION['resultado_solicitud'])) {
+                        // Mostrar el mensaje de resultado
+                        echo "<h1>{$_SESSION['resultado_solicitud']}</h1>";
+                    } else {
+                        // Si la variable $resultado no está definida, mostrar un mensaje de error
+                        echo "<h1>No se ha recibido ningún resultado.</h1>";
+                    }
+                    ?>
+                    <div class="mt-3">
+                        <a href="index.php?action=solicitud_asuntos_propios" class="btn btn-primary">Volver a la página
+                            de solicitud de asuntos propios</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- view/resultado_baja_accidente_enfermedad.php
+++ view/resultado_baja_accidente_enfermedad.php
@@ -12,21 +12,25 @@ if (!isset($_SESSION['correo'])) {
 }
 ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 offset-md-2 content">
-            <?php
-            // Verificar si la variable $resultado está definida en la sesión
-            if (isset($_SESSION['resultado_solicitud'])) {
-                // Mostrar el mensaje de resultado
-                echo "<h1 class='text-center'>{$_SESSION['resultado_solicitud']}</h1>";
-            } else {
-                // Si la variable $resultado no está definida, mostrar un mensaje de error
-                echo "<h1 class='text-center'>No se ha recibido ningún resultado.</h1>";
-            }
-            ?>
-            <div class="text-center mt-3">
-                <a href="index.php?action=logeado" class="btn btn-primary">Volver a la página principal</a>
+<div class="container mt-5 mb-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body text-center">
+                    <?php
+                    // Verificar si la variable $resultado está definida en la sesión
+                    if (isset($_SESSION['resultado_solicitud'])) {
+                        // Mostrar el mensaje de resultado
+                        echo "<h1>{$_SESSION['resultado_solicitud']}</h1>";
+                    } else {
+                        // Si la variable $resultado no está definida, mostrar un mensaje de error
+                        echo "<h1>No se ha recibido ningún resultado.</h1>";
+                    }
+                    ?>
+                    <div class="mt-3">
+                        <a href="index.php?action=logeado" class="btn btn-primary">Volver a la página principal</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
